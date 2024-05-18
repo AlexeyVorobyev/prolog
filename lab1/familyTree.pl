029b28333ca5dc6c013200fb05118ba83e3a2511
a.vorobyev <mister.alex49@yandex.ru>
2024-05-18 10:19:02 +0300

    LAB1 TASK3
    Построены предикаты:
    Предикат, проверяющий является ли X - бабушкой Y
    Предикат, выводящий всех бабушек X
    Предикат, проверяющий является ли X,Y - дедом и внуком или внуком и дедом
    Предикат, определяющий то, что X - сиблинг Y
    Предикат, проверяющий является ли X - дядей Y
    Предикат, проверяющий является ли X - дядей Y,
    но без использования готового предиката
    Предикат, выводящий всех дядей X

--- lab1/familyTree.pl
+++ lab1/familyTree.pl
@@ -100,3 +100,47 @@ sister(X,Y):-
 	X \= Y.
 /*Предикат, выводящий всех сестёр X*/
 sisters(X):- sister(Y,X), print(Y), nl, fail.
+
+
+/*Задание 3*/
+
+
+/*Предикат, проверяющий является ли X - бабушкой Y*/
+grand_ma(X,Y):-
+	woman(X),
+	parent(X,Z),
+	parent(Z,Y).
+/*Предикат, выводящий всех бабушек X*/
+grand_mas(X):- grand_ma(Y,X), print(Y), nl, fail.
+
+/*Предикат, проверяющий является ли X,Y - дедом и внуком или внуком и дедом*/
+grand_pa_and_son(X,Y):-
+	man(X),
+	man(Y),
+	(
+		parent(X,Z),
+		parent(Z,Y);
+		parent(Y,Z),
+		parent(Z,X)
+	).
+
+/*Предикат, определяющий то, что X - сиблинг Y*/
+sibling(X,Y):-
+	parent(Z,X),
+	parent(Z,Y),
+	X \= Y.
+/*Предикат, проверяющий является ли X - дядей Y*/
+uncle(X,Y):-
+	man(X),
+	parent(Z,Y),
+	sibling(X,Z).
+/*Предикат, проверяющий является ли X - дядей Y,
+но без использования готового предиката*/
+uncle_ry(X,Y):-
+	man(X),
+	parent(Z,Y),
+	parent(W,X),
+	parent(W,Z),
+	X \= Z.
+/*Предикат, выводящий всех дядей X*/
+uncle(X):- uncle(Y,X), print(Y), nl, fail.
